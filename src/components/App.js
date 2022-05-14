52cb3ec18b933f9dae023244b41648a1df24dcc5
annomao <omaoannkerubo@gmail.com>
2022-05-14 20:18:44 +0300

    add delete functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,22 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTask] = useState(TASKS)
+
+  function handleDeleteTask(deletedTask){
+    const newTaskList = tasks.filter((task)=>{
+      return task.text !== deletedTask.text
+    })
+    setTask(newTaskList)
+  }
+
+  
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={tasks} onDeleteTask={handleDeleteTask}/>
     </div>
   );
 }
